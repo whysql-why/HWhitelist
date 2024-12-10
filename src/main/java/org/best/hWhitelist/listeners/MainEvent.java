e26842d94df0f1282abb683b9ded722b9c921ddb
whysql-why <mbhv1122@gmail.com>
2024-12-10 17:54:38 -0500

    fix

--- src/main/java/org/best/hWhitelist/listeners/MainEvent.java
+++ src/main/java/org/best/hWhitelist/listeners/MainEvent.java
@@ -20,31 +20,24 @@ public class MainEvent implements Listener {
         Boolean kick = false;
         Boolean stay = false;
         String hostname = e.getHostname();
-        if(anything_but_these_enabled){
+        if(anything_but_these_enabled) {
             List<String> first_hosts = (List<String>) plugin.getConfig().getList("anything_but_these.hosts");
-            for(String host : first_hosts){
-                if(hostname.equalsIgnoreCase(host)){
+            for (String host : first_hosts) {
+                if (hostname.equalsIgnoreCase(host)) {
                     kick = true;
                 }
             }
-            if(only_from_this_enabled){
+        } else if(only_from_this_enabled){
                 List<String> second_hosts = (List<String>) plugin.getConfig().getList("only_from_this.hosts");
-                plugin.getLogger().warning("[!] You have both options set to 'Enabled'");
-                plugin.getLogger().warning("[!] This might cause issues in the future.");
+                if(anything_but_these_enabled) {
+                    plugin.getLogger().warning("[!] You have both options set to 'Enabled'");
+                    plugin.getLogger().warning("[!] This might cause issues in the future.");
+                }
                 for(String host : second_hosts){
                     if(!hostname.equalsIgnoreCase(host)){
                         kick = true;
                     }
                 }
-            }
-        }
-        if(only_from_this_enabled){
-            List<String> hosts = (List<String>) plugin.getConfig().getList("only_from_this.hosts");
-            for (String host : hosts){
-                if(!hostname.equalsIgnoreCase(host)){
-                    kick = true;
-                }
-            }
         }
         if(kick && !stay){
             String kick_message = plugin.getConfig().getString("kick_message").replace("&", "§");
